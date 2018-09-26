0fb3ed7345a57c5c965e73c7c090a1fcaff80d8e
ramadhar <k.ramadhar88@gmail.com>
2018-09-26 08:55:22 +0530

    Create HelloWorld.py

---
+++ Basic/HelloWorld.py
@@ -0,0 +1,27 @@
+#print('Hello World')
+
+# variable initialization and mathematical operation
+
+a=5
+b=6
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+print(a%b)
+
+print(b%a)
+
+print(b//a)
+print(b/a)
+
+x=2*4+3/2+(6+7)
+print(x)
+
+print("What is : ")
+
+
+
+
